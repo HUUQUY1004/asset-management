f61e4ff32c42b872b6bb702eb037aa22fffd6db1
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2025-03-21 10:01:25 +0700

    merge code

--- assetmanagement/src/main/java/com/assetmanagement/assetmanagement/repository/AssetRepository.java
+++ assetmanagement/src/main/java/com/assetmanagement/assetmanagement/repository/AssetRepository.java
@@ -1,9 +1,12 @@
-package com.assetmanagement.assetmanagement.lebaphung;
+package com.assetmanagement.assetmanagement.repository;
 
 import com.assetmanagement.assetmanagement.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+    List<Asset> findByStatus(String status);
 }
--- assetmanagement/src/main/java/com/assetmanagement/assetmanagement/service/AssetService.java
+++ assetmanagement/src/main/java/com/assetmanagement/assetmanagement/service/AssetService.java
@@ -1,7 +1,8 @@
-package com.assetmanagement.assetmanagement.createAsset.service;
+package com.assetmanagement.assetmanagement.service;
 
 import com.assetmanagement.assetmanagement.entity.Asset;
-import com.assetmanagement.assetmanagement.createAsset.repository.AssetRepository;
+import com.assetmanagement.assetmanagement.repository.AssetRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,16 +10,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AssetService {
-
-    private final AssetRepository assetRepository;
-    //command
-
     @Autowired
-    public AssetService(AssetRepository assetRepository) {
-        this.assetRepository = assetRepository;
-    }
+    private AssetRepository assetRepository;
 
+    public void deleteAsset(Long id) {
+        Asset asset = assetRepository.findById(id).orElseThrow(() -> new RuntimeException("Asset not found"));
+        asset.setDeleted(true);
+        assetRepository.save(asset);
+    }
     public Asset createAsset(Asset asset) {
         asset.setLastUpdated(LocalDateTime.now());
         return assetRepository.save(asset);
@@ -28,4 +29,3 @@ public class AssetService {
         return assetRepository.findByStatus(status);
     }
 }
-
