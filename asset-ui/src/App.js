974f405b3cd752054eb75464e30ae25298bb8dc3
Lê Quốc Trung <115451384+Danny2611@users.noreply.github.com>
2025-03-21 14:42:26 +0700

    add header and footer

--- asset-ui/src/App.js
+++ asset-ui/src/App.js
@@ -2,16 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import { routes } from "./router";
-
+import Layout from "./component/layout/Layout";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+     
+     <Routes>
         {routes.map((item, index) => {
           const Page = item.Component;
-          return <Route path={item.path} element={<Page />} />;
+          return <Route path={item.path} element={
+            <Layout>
+                <Page />
+            </Layout>
+          } />;
         })}
       </Routes>
+   
+      
     </BrowserRouter>
   );
 }
