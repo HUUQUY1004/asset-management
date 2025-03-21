156e4302eb3a93e74b895656d94c9ceb6e4f8b21
VoPhong2503 <voquocphong6678@gmail.com>
2025-03-21 10:20:14 +0700

    update asset

--- assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/AssetController.java
+++ assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/AssetController.java
@@ -1,5 +1,6 @@
 package com.assetmanagement.assetmanagement.controller;
 
+import com.assetmanagement.assetmanagement.dto.UpdateAssetRequets;
 import com.assetmanagement.assetmanagement.entity.Asset;
 import com.assetmanagement.assetmanagement.repository.AssetRepository;
 import com.assetmanagement.assetmanagement.service.AssetService;
@@ -15,24 +16,34 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AssetController {
     @Autowired
-    private  AssetService assetService;
+    private AssetService assetService;
     @Autowired
-    private  AssetRepository assetRepository;
+    private AssetRepository assetRepository;
 
     //user story 4
     @GetMapping("/status/{status}")
     public ResponseEntity<List<Asset>> getAssetsByStatus(@PathVariable String status) {
         return ResponseEntity.ok(assetService.getAssetsByStatus(status));
     }
+
     @PostMapping("/{assetId}")
     public ResponseEntity<String> deletedAsset(@PathVariable("assetId") Long assetId) {
         assetService.deleteAsset(assetId);
         return ResponseEntity.ok("Deleted asset " + assetId);
     }
+
     @PostMapping("/create")
     public ResponseEntity<Asset> createAsset(@RequestBody Asset asset) {
         Asset newAsset = assetService.createAsset(asset);
         return ResponseEntity.ok(newAsset);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Asset> updateAsset(
+            @PathVariable Long id,
+            @RequestBody UpdateAssetRequets dto) {
+        Asset updatedAsset = assetService.updateAsset(id, dto);
+        return ResponseEntity.ok(updatedAsset);
     }
 
 }
