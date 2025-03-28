557c975fe899bc4c89c614151a182383c7bde7ac
Pblues <pbluest.rong@gmail.com>
2025-03-28 09:50:56 +0700

    first commit
    # Conflicts:
    #	assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/UserAccountController.java

--- assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/BorrowController.java
+++ assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/BorrowController.java
@@ -1,6 +1,7 @@
 package com.assetmanagement.assetmanagement.controller;
 
 import com.assetmanagement.assetmanagement.entity.Asset;
+import com.assetmanagement.assetmanagement.entity.Notify;
 import com.assetmanagement.assetmanagement.repository.NotifyRepository;
 import com.assetmanagement.assetmanagement.response.Response;
 import com.assetmanagement.assetmanagement.security.JwtService;
@@ -26,14 +27,20 @@ public class BorrowController {
     public ResponseEntity<Response> borrowAsset(
             @PathVariable("assetId") Long assetId,
             @RequestHeader("Authorization") String jwt
-    ){
+    ) {
         Optional<Asset> assetOptional = assetService.getAssetById(assetId);
-
+        jwt = jwt.substring(7).trim();
         if (assetOptional.isPresent()) {
             Asset asset = assetOptional.get(); // Lấy giá trị Asset từ Optional
             System.out.println("AssetId: " + assetId);
             System.out.println("Asset: " + asset.getName());
-            System.out.println("email" + jwtService.extractUserName(jwt));
+
+
+            Notify notify = new Notify();
+            notify.setMessage(jwtService.extractUserName(jwt) + " muốn mượn tài sản " + asset.getName());
+            notify.setRedirect("/approve/asset/" + asset.getId());
+            notifyRepository.save(notify);
+
             Response response = new Response();
             response.setMessage("Mượn tài sản thành công");
             response.setStatus(200);
--- assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/UserAccountController.java
+++ assetmanagement/src/main/java/com/assetmanagement/assetmanagement/controller/UserAccountController.java
@@ -1,17 +1,30 @@
 package com.assetmanagement.assetmanagement.controller;
 
+import com.assetmanagement.assetmanagement.dto.UserAccountResponse;
 import com.assetmanagement.assetmanagement.entity.UserAccount;
 import com.assetmanagement.assetmanagement.service.UserAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user-accounts")
 public class UserAccountController {
+
+    @GetMapping("/info")
+    public ResponseEntity<UserAccountResponse> getUserAccount(Authentication authentication) {
+        UserAccount userAccount = (UserAccount) authentication.getPrincipal();
+        return new ResponseEntity<>(UserAccountResponse.builder()
+                .id(userAccount.getId())
+                .username(userAccount.getUsername())
+                .role(userAccount.getRole())
+                .status(userAccount.getStatus())
+                .build(), HttpStatus.OK);
+    }
 
     @Autowired
     private UserAccountService userAccountService;
